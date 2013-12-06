33f55d58ec8f78e17ad7f3af9082c3d12f86f3b0
Philip Callender <philcal@mac.com>
2013-12-06 18:57:47 +0800

    Added uploading a Category Mapping from a CSV file.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+var fs = require("fs");
+
 
 // Check the command line prameters
 if (process.argv.length === 6 && process.argv[2] === 'categoryMap' && endsWith(process.argv[5], '.csv')){
@@ -8,7 +10,7 @@ if (process.argv.length === 6 && process.argv[2] === 'categoryMap' && endsWith(p
 	var accessToken = process.argv[3];
 	var sourceId = process.argv[4];
 	var csvFile = process.argv[5];
-	require('./code/categoryMap_csv').load(accessToken, sourceId, csvFile);
+	require('./code/categoryMap_csv').load(accessToken, sourceId, csvFile); // async
 } else {
 	
 	// Unrecognised command
@@ -20,7 +22,7 @@ if (process.argv.length === 6 && process.argv[2] === 'categoryMap' && endsWith(p
 
 
 
-process.exit(0);
+//process.exit(0);
 // Finished
 
 /**
--- code/categoryMap_csv.js
+++ code/categoryMap_csv.js
@@ -1,5 +1,6 @@
 var csv = require("fast-csv"),
-	fs = require("fs");
+	fs = require("fs"),
+	request = require('request');
 
 
 
@@ -7,28 +8,72 @@ exports.load = function(accessToken, sourceId, csvfile) {
 	
 	console.log("categoryMap_csv.load(" + accessToken + ", " + sourceId + ", " + csvfile + ")");
 	
-	var stream = fs.createReadStream(csvfile);
-console.log('here we go...')
+	pass1_validate(accessToken, sourceId, csvfile);
+}
 
-	// Step 1 - and verify the CSV file
+/**
+ *	First. Check the CSV has the correct heading line, and the right number of values
+ *	on each line. Also, check that the TEA categories are all valid (ZZZZ NOT YET).
+ *	@return Async function
+ */
+function pass1_validate(accessToken, sourceId, csvfile) {
+
+	console.log("Pass 1: Validation")
+	var stream = fs.createReadStream(csvfile);
+	stream.on('error', function (error) {console.log("Caught", error);bomb('CSV file error.')});
+
+	// Verify each line of the CSV file
 	var count = 0;
-	var map = { };
 	csv(stream)
 		.on("data", function(data){
-console.log('have data row', data)
+			//console.log('have data row', data)
 			if (count++ === 0) {
 
 				// Header row
 				if (data[0] != "source_category" || data[1] != "tea_category") {
-					throw new Error('First line must contain "source_category,tea_category"');
+					bomb('First line must contain "source_category,tea_category"');
 				}
 				return;
 			} else {
 
 				// Non-header row
 				if (data.length != 2) {
-					throw new Error('Line ' + count + ' should contain 2 fields');
+					bomb('Line ' + count + ' should contain 2 fields');
 				}
+			}
+		})
+		.on("end", function(){
+
+			// Finished loading the mapping. Send it to TEA.
+			console.log("  - pass 1 complete")
+			console.log("  - header line and " + count + " data lines passed validation.")
+			pass2_loadMap(accessToken, sourceId, csvfile);
+		})
+		.on("error", function(){
+			console.log("Pass 1: error loading CSV file.")
+		})
+		.parse();
+}
+
+/**
+ *	Second pass. Load the CSV into a Javascript object, using the source_category
+ *	as the property name, and the tea_category as the value.
+ *	@return Async function
+ */
+function pass2_loadMap(accessToken, sourceId, csvfile) {
+	console.log("Pass 2: Load mapping")
+	var stream = fs.createReadStream(csvfile);
+	stream.on('error', function (error) {console.log("Caught", error);bomb('CSV file error.')});
+
+	// Load the mappings into a Javascript object
+	var count = 0;
+	var map = { };
+	
+	csv(stream)
+		.on("data", function(data){
+			if (count++ > 0) {
+
+				// Non-header row
 				var source_category = data[0];
 				var tea_category = data[1];
 				map[source_category] = tea_category;
@@ -37,11 +82,65 @@ console.log('have data row', data)
 		.on("end", function(){
 
 			// Finished loading the mapping. Send it to TEA.
-			console.log("map=>\n", map)
+			console.log('  - pass 2 complete');
+			pass3_upload(accessToken, sourceId, map);
 		})
 		.on("error", function(){
-			console.log("Got an error loading CSV file.")
+			bomb("Pass 2: error loading CSV file.")
 		})
 		.parse();
+}
 
+/**
+ *	Third pass. Upload the new category mappings into TEA using the RESTful API.
+ *	@return Async function
+ */
+function pass3_upload(accessToken, sourceId, map) {
+	console.log("Pass 3: Upload to TEA")
+	
+	var json = {
+		access_token: accessToken,
+		source_id: sourceId,
+//		  "overwrite_existing": true,
+		map: map
+	};
+	
+	// Send it to TEA via the RESTful API
+	var url = 'http://localhost:8080/categoryMap';
+	
+	console.log('  - calling TEA to update categoryMap:')
+	// console.log('  url=>' + url + ' (POST)')
+	// console.log('  data=>\n', json)
+	
+	request({
+		method: 'POST',
+		url: url,
+		json: json
+	}, function (error, response, body) {
+
+		//console.log("Back, error="+error, body);
+
+		if (error) {
+	        bomb("Unexpected error: ", error);
+		} else if (response.statusCode != 200) {
+	        //console.log("Body is " + body);
+	        bomb("Unexpected status code " + response.statusCode);
+		} else {
+			if (body.response != 'Success') {
+				bomb('ERROR: response="' + body.response + '", message="' + body.message + '"');
+			}
+			console.log('  - upload complete');
+			console.log("\nStatus: OK\n");
+			process.exit(0);
+		}
+	});
+}
+
+/**
+ *	Write out a message, and exit with error status
+ */
+function bomb(message) {
+	console.log(message);
+	console.log("\nStatus: ERROR\n");
+	process.exit(1);
 }
